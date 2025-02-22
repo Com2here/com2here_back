a24130deff7a81116aec429849f4f272ddc04d2b
hhjkhhbgkugjnbj <ck8901ck@gmail.com>
2025-02-23 05:00:43 +0900

    Refect

--- src/main/java/com/com2here/com2hereback/service/UserService.java
+++ src/main/java/com/com2here/com2hereback/service/UserService.java
@@ -1,34 +1,12 @@
 package com.com2here.com2hereback.service;
 
-import com.com2here.com2hereback.domain.User;
-import com.com2here.com2hereback.repository.UserRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
+import com.com2here.com2hereback.dto.UserRequestDto;
+import com.com2here.com2hereback.dto.UserResponseDto;
 
-import lombok.RequiredArgsConstructor;
+public interface UserService {
 
-@RequiredArgsConstructor
-@Service
-public class UserService {
-
-    private final UserRepository userRepository;
-
-    // 비밀번호 암호화 및 중복 이메일 체크 후 회원가입 처리
-    public User create(User user) {
-        return userRepository.save(user);
-    }
-
-    // 이메일로 유저 정보 찾기 및 비밀번호 검증
-    public User getByCredentials(String email, String password, PasswordEncoder passwordEncoder) {
-        User user = userRepository.findByEmail(email);
-        if (user != null && passwordEncoder.matches(password, user.getPassword())) {
-            return user;
-        }
-        return null;
-    }
-
-    // 이메일 중복 체크
-    public boolean existsByEmail(String email) {
-        return userRepository.existsByEmail(email);
-    }
+    // 회원가입
+    String RegisterUser(UserRequestDto userRequestDto);
+    // 로그인
+    UserResponseDto LoginUser(UserRequestDto userRequestDto);
 }
