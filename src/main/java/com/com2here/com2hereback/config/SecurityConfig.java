b390aa0359fb0ac7e063193664d423adc35ef2b5
dbstj589 <lysfox8@gmail.com>
2025-04-09 19:59:58 +0900

    refactor

--- src/main/java/com/com2here/com2hereback/config/SecurityConfig.java
+++ src/main/java/com/com2here/com2hereback/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(authz -> authz
                 .requestMatchers(
-                    "/api/v1/email/*",
+                    "/api/v1/email/**",
                     "/api/v1/oauth/*",
                     "/api/v1/user/register",
                     "/api/v1/user/login"
